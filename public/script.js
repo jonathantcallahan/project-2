3e512115d7f992de40518de954685e60c23b76b8
unknown <jtc7537@gmail.com>
2018-03-24 14:20:55 -0400

    created timer counter

--- public/script.js
+++ public/script.js
@@ -1,5 +1,13 @@
 $(document).ready(() => {
     //alert(Date.now())
+    function secondCounter(time){
+        var newTime = parseInt(time);
+        function displayNum(){
+            newTime += .008
+            $('#hunger-level').text(`${newTime.toFixed(1)} minutes have passed since last feeding`)
+        };
+        setInterval(displayNum, 500)
+    }
 
     //ajax call to get timestamp from database
     $.ajax({
@@ -9,7 +17,8 @@ $(document).ready(() => {
         console.log(data)
         var elapsedTime = ((Date.now() - parseFloat(data[0].lastFed))/1000)/60
         console.log(`${elapsedTime} minutes have pased since last fed`)
-        $('#hunger-level').text(`${elapsedTime} minutes have passed since last feeding`)
+        $('#hunger-level').text(`${elapsedTime.toFixed(2)} minutes have passed since last feeding`)
+        secondCounter(elapsedTime.toFixed(2))
     }).catch((err)=>console.log(err))
 
 
@@ -30,7 +39,7 @@ $(document).ready(() => {
             }
         }).then(data => {
             console.log(data);
-            window.location.reload(true);
+            location.reload(true);
         }).catch(err => console.log(err))
 
         // $.post('/my-pet', {
